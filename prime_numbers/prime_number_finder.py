6278a514bef6c0528475097e41f8bdf647e43c4a
player89087 <edar.horst@gmail.com>
2025-05-28 23:04:37 +0200

    slight adjustements, added time tracking, numbers get saved to a file, every 100 number is printed out to increase perfomance and as safety for the user

--- prime_numbers/prime_number_finder.py
+++ prime_numbers/prime_number_finder.py
@@ -16,7 +16,7 @@ amount = args.n
 
 
 prime_numb = []
-
+start = time.time()
 for i in range(2,amount):
     is_prime_number = True 
     for x in range(2,amount):
@@ -27,7 +27,14 @@ for i in range(2,amount):
         else:
             y += 1
     if is_prime_number == True:
-        prime_numb.append(i)
-
+        if i % 100 == 0:
+            print(f"Prime number found: {i}")
+        else:
+            prime_numb.append(i)
+end = time.time()
+dur = end - start
 for z in range(0,len(prime_numb)):
-    print(prime_numb[z], end = "  ")
+    with open("prime_numb.txt", "a") as file: 
+        file.write(str(prime_numb[z]) + "\n")
+    
+print(f"Needed time: {dur} seconds")
