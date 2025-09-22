07f4bb65e9f92f09937f94e019a2234e9a492a95
player89087 <edar.horst@gmail.com>
2025-09-22 19:18:17 +0200

    removed i % 100 as it was annoying

--- Mathe_Olympiade_2025/mathe_olympiade651012.py
+++ Mathe_Olympiade_2025/mathe_olympiade651012.py
@@ -7,7 +7,7 @@ for i in range(0,1000):
                 solutions_1.append(i)
                 solutions_1.append(x)
 
-
+print(solutions_1)
 # all solutions for a^2 + b^2 = 340
 solutions_2 = []
 for i in range(0,1000):
@@ -20,13 +20,14 @@ print(solutions_2)
 
 solutions_3 = []
 
-for i in range(0,100000):
-    for x in range(0,100000):
+for i in range(0,8024):
+    for x in range(0,8024):
         if i**2 + x**2 == 8024:
                 solutions_3.append(i)
                 solutions_3.append(x)
-        if i % 100 == 0: # indicates that the programm is running
-            print(i)
+        
+
+print(solutions_3)
 
 # no result back --> there is no solution
 
