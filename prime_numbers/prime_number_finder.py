d67501a1f7c20dc23845e424e89b93778f7bdc0e
player89087 <edar.horst@gmail.com>
2025-12-12 18:39:08 +0100

    fixed problems, now just prime number

--- prime_numbers/prime_number_finder.py
+++ prime_numbers/prime_number_finder.py
@@ -7,7 +7,7 @@ sys.set_int_max_str_digits(1000000000)
 
 
 parser = argparse.ArgumentParser(
-    description="fibonacci sequence"
+    description="primer numbers"
 )
 parser.add_argument("-n", required=True, type=int) 
 args = parser.parse_args()
@@ -19,18 +19,18 @@ prime_numb = []
 start = time.time()
 for i in range(2,amount):
     is_prime_number = True 
-    for x in range(2,amount):
-        y = 2
+    y = 2
+    
+    for x in range(2,i):
         if i % y == 0:
             is_prime_number = False
             break
         else:
             y += 1
+    
     if is_prime_number == True:
-        if i % 100 == 0:
-            print(f"Prime number found: {i}")
-        else:
-            prime_numb.append(i)
+        print(i)
+        prime_numb.append(i)    
 end = time.time()
 dur = end - start
 for z in range(0,len(prime_numb)):
